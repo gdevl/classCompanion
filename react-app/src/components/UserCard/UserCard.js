a348665461aa7edcc9d19ddef5a1a99186c4c6bf
tamagri <tamagrijr@gmail.com>
2020-11-24 14:32:25 -0600

    idk

--- react-app/src/components/UserCard/UserCard.js
+++ react-app/src/components/UserCard/UserCard.js
@@ -14,6 +14,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import 'UserCard.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
