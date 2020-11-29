379a53b2cefb2d789748920e083b5c4ea4c03381
tamagri <tamagrijr@gmail.com>
2020-11-29 15:20:32 -0600

    instructor layout for classrooms finished

--- react-app/src/components/InstructorClassroomDashboard/AnswerModal/AnswerModal.js
+++ react-app/src/components/InstructorClassroomDashboard/AnswerModal/AnswerModal.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchClassrooms, setUserClasses } from '../../../../src/store/users'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,8 +10,10 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default function AnswerModal({ props }) {
-  const currentUser= useSelector(state => state.store.current_user)
-  const currentClass= useSelector(state => state.store.current_class)
+  const dispatch = useDispatch();
+
+  const currentUser = useSelector(state => state.store.current_user)
+  const currentClass = useSelector(state => state.store.current_class)
 
   const [answer, setAnswer] = React.useState('');
   const handleAnswerChange = (event) => {
@@ -29,7 +32,11 @@ export default function AnswerModal({ props }) {
         answer: 'dismissed'
       }),
     });
-    if(response.ok) props.setOpen(null)
+    if (response.ok) {
+      const classrooms = await fetchClassrooms(currentUser.id);
+      dispatch(setUserClasses(classrooms))
+      props.setOpen(null)
+    }
 
     // props.setOpen(null)
   }
@@ -46,16 +53,20 @@ export default function AnswerModal({ props }) {
         answer: answer
       }),
     });
-    if(response.ok) props.setOpen(null)
+    if (response.ok) {
+      const classrooms = await fetchClassrooms(currentUser.id);
+      dispatch(setUserClasses(classrooms))
+      props.setOpen(null)
+    }
 
     // props.setOpen(null)
   }
 
-  if(!props) return null
+  if (!props) return null
   return (
     <>
 
-        <Dialog open={props.open == props.user.id} onClose={() => props.setOpen(null)} aria-labelledby="form-dialog-title">
+      <Dialog open={props.open == props.user.id} onClose={() => props.setOpen(null)} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">{`${props.user.first_name}'s Question`}</DialogTitle>
         <DialogContent>
           <DialogContentText>
