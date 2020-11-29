c713c21bd76787f699616ca6757902c062581496
tamagri <tamagrijr@gmail.com>
2020-11-29 13:46:33 -0600

    cut comments

--- react-app/src/components/InstructorClassroomDashboard/ClassroomLayout/ClassroomLayout.js
+++ react-app/src/components/InstructorClassroomDashboard/ClassroomLayout/ClassroomLayout.js
@@ -24,37 +24,6 @@ export default function ClassroomLayout({ props }) {
     for(let i = 0; i < props.groups.length; i++){
       groups.push(`Group ${i+1}`)
     }
-    // let members = [];
-    // for(let i = 0; i < )
-    // //shuffle array function
-    // function shuffleArray(arr) {
-    //   for (let i = arr.length - 1; i > 0; i--) {
-    //     const j = Math.floor(Math.random() * (i + 1));
-    //     [arr[i], arr[j]] = [arr[j], arr[i]];
-    //   }
-    // }
-    // let arr = [1, 2, 3, 4, 5];
-    // shuffleArray(arr);
-
-    //determine the number of groups
-    // const numGroups = Math.ceil(props.users.length / props.groups.groupSize)
-
-    //create an array of groups to map
-    // let users = props.users.map(user => user)
-
-
-    // for(let i = 0; i < groups.length; i++) {
-    //   let groupMembers = [];
-    //   for(let j = 0; j < props.groups.groupSize; j++) {
-    //     shuffleArray(users)
-    //     if(users.length > 0){
-    //       let member = users.pop()
-    //       groupMembers.push(member)
-    //     }
-    //   }
-    //   members.push(groupMembers)
-    // }
-
 
     return (
       <>
