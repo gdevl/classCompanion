95435070ee9d1d77d9511904a4f1298cd7f01cb7
gdevl <throwawayemailaddy@protonmail.com>
2021-01-04 21:55:43 -0500

    updated seeds.py

--- app/seeds/seeds.py
+++ app/seeds/seeds.py
@@ -1,70 +1,336 @@
 from werkzeug.security import generate_password_hash
-from app.models import db, User, Instructor, Student
+from app.models import db, User, Instructor, Student, Classroom
 
 # Adds a demo user, you can add other users here if you want
 
 
 def seed_users():
 
-    # demo = Instructor(
-    #     username='Demo',
-    #     first_name="de",
-    #     last_name="mo",
-    #     email='demo@aa.io',
-    #     password='password',
-    # )
-
-    demo_student = Student(
-        username='Student',
-        first_name="Stu",
-        last_name="Dent",
-        email='student@aa.io',
-        password='password',
+    deanPelton = Instructor(
+        username="cpelton",
+        first_name="Craig",
+        last_name="Pelton",
+        email="cpelton@greendale.edu",
+        password="password",
     )
 
-    demo_student2 = Student(
-        username='Student2',
-        first_name="Stu2",
-        last_name="Dent2",
-        email='student2@aa.io',
-        password='password',
+    # students
+
+    jeff = Student(
+        username="jwinger",
+        first_name="Jeff",
+        last_name="Winger",
+        email="jwinger@greendale.edu",
+        password="password",
     )
 
-    demo_student3 = Student(
-        username='Student3',
-        first_name="Stu3",
-        last_name="Dent3",
-        email='student3@aa.io',
-        password='password',
+    abed = Student(
+        username="anadir",
+        first_name="Abed",
+        last_name="Nadir",
+        email="anadir@greendale.edu",
+        password="password",
     )
 
-    demo_student4 = Student(
-        username='Student4',
-        first_name="Stu4",
-        last_name="Dent4",
-        email='student4@aa.io',
-        password='password',
+    britta = Student(
+        username="bperry",
+        first_name="Britta",
+        last_name="Perry",
+        email="bperry@greendale.edu",
+        password="password",
     )
 
-    demo_student5 = Student(
-        username='Student5',
-        first_name="Stu5",
-        last_name="Dent5",
-        email='student5@aa.io',
-        password='password',
+    shirley = Student(
+        username="sbennett",
+        first_name="Shirley",
+        last_name="Bennett",
+        email="sbennett@greendale.edu",
+        password="password",
     )
 
-    demo_student6 = Student(
-        username='Student6',
-        first_name="Stu6",
-        last_name="Dent6",
-        email='student6@aa.io',
-        password='password',
+    annie = Student(
+        username="aedison",
+        first_name="Annie",
+        last_name="Edison",
+        email="aedison@greendale.edu",
+        password="password",
     )
 
-    db.session.add_all([demo_student2, demo_student3, demo_student4, demo_student5, demo_student6])
+    troy = Student(
+        username="tbarnes",
+        first_name="Troy",
+        last_name="Barnes",
+        email="tbarnes@greendale.edu",
+        password="password",
+    )
+
+    pierce = Student(
+        username="phawthorne",
+        first_name="Pierce",
+        last_name="Hawthorne",
+        email="phawthorne@greendale.edu",
+        password="password",
+    )
+
+    starburns = Student(
+        username="aosbourne",
+        first_name="Alex",
+        last_name="Osbourne",
+        email="aosbourne@greendale.edu",
+        password="password",
+    )
+
+    leonard = Student(
+        username="lrodriguez",
+        first_name="Leonard",
+        last_name="Rodriguez",
+        email="lrodriguez@greendale.edu",
+        password="password",
+    )
+
+    vaughn = Student(
+        username="vmiller",
+        first_name="Vaughn",
+        last_name="Miller",
+        email="vmiller@greendale.edu",
+        password="password",
+    )
+
+    garrett = Student(
+        username="glambert",
+        first_name="Garrett",
+        last_name="Lambert",
+        email="glambert@greendale.edu",
+        password="password",
+    )
+
+    rich = Student(
+        username="rstephenson",
+        first_name="Rich",
+        last_name="Stephenson",
+        email="rstephenson@greendale.edu",
+        password="password",
+    )
+
+    # faculty
+
+    benChang = Instructor(
+        username="bchang",
+        first_name="Ben",
+        last_name="Chang",
+        email="bchang@greendale.edu",
+        password="password",
+    )
+
+    ianDuncan = Instructor(
+        username="iduncan",
+        first_name="Ian",
+        last_name="Duncan",
+        email="iduncan@greendale.edu",
+        password="password",
+    )
+
+    michelleSlater = Instructor(
+        username="mslater",
+        first_name="Michelle",
+        last_name="Slater",
+        email="mslater@greendale.edu",
+        password="password",
+    )
+
+    seanGarrity = Instructor(
+        username="sgarrity",
+        first_name="Sean",
+        last_name="Garrity",
+        email="sgarrity@greendale.edu",
+        password="password",
+    )
+
+    juneBauer = Instructor(
+        username="jbauer",
+        first_name="June",
+        last_name="Bauer",
+        email="jbauer@greendale.edu",
+        password="password",
+    )
+
+    eusticeWhitman = Instructor(
+        username="ewhitman",
+        first_name="Eustice",
+        last_name="Whitman",
+        email="ewhitman@greendale.edu",
+        password="password",
+    )
+
+    marshallKane = Instructor(
+        username="mkane",
+        first_name="Marshall",
+        last_name="Kane",
+        email="mkane@greendale.edu",
+        password="password",
+    )
+
+
+    # classes
+
+    spanish101 = Classroom(
+        name="Spanish 101",
+        description="Intro to Spanish",
+    )
+    spanish101.instructors.append(benChang)
+    spanish101roster = [
+        jeff,
+        abed,
+        britta,
+        shirley,
+        annie,
+        troy,
+        pierce,
+        starburns,
+        leonard
+    ]
+    spanish101.students.extend(spanish101roster)
+
+    psychology101 = Classroom(
+        name="Psychology 101",
+        description="Intro to Psychology",
+    )
+    psychology101.instructors.append(ianDuncan)
+    psychology101roster = [
+        abed,
+        annie,
+        troy,
+        starburns,
+        garrett,
+        rich,
+        vaughn
+    ]
+    psychology101.students.extend(psychology101roster)
+
+    statistics101 = Classroom(
+        name="Statistics 101",
+        description="Intro to Statistics",
+    )
+    statistics101.instructors.append(michelleSlater)
+    statistics101roster = [
+        jeff,
+        abed,
+        troy,
+        pierce,
+        starburns,
+        leonard,
+        rich
+    ]
+    statistics101.students.extend(statistics101roster)
+
+    accounting101 = Classroom(
+        name="Accounting 101",
+        description="Intro to Accounting",
+    )
+    accounting101.instructors.append(eusticeWhitman)
+    accounting101roster = [
+        jeff,
+        shirley,
+        annie,
+        pierce,
+        starburns,
+        leonard,
+        rich
+    ]
+    accounting101.students.extend(accounting101roster)
+
+    anthropology101 = Classroom(
+        name="Anthropology 101",
+        description="Intro to Anthropology",
+    )
+    anthropology101.instructors.append(juneBauer)
+    anthropology101roster = [
+        jeff,
+        abed,
+        britta,
+        shirley,
+        annie,
+        troy,
+        pierce,
+        starburns,
+        leonard,
+        rich,
+        vaughn
+    ]
+    anthropology101.students.extend(anthropology101roster)
+
+    conspiracyTheories101 = Classroom(
+        name="Conspiracy Theories 101",
+        description="Intro to Conspiracy Theories",
+    )
+    conspiracyTheories101.instructors.append(seanGarrity)
+    conspiracyTheories101roster = [
+        jeff,
+        annie
+    ]
+    conspiracyTheories101.students.extend(conspiracyTheories101roster)
+
+    biology101 = Classroom(
+        name="Biology 101",
+        description="Intro to Biology",
+    )
+    biology101.instructors.append(marshallKane)
+    biology101roster = [
+        jeff,
+        abed,
+        britta,
+        shirley,
+        annie,
+        troy,
+        pierce,
+        starburns,
+        leonard,
+        rich,
+        vaughn
+    ]
+    biology101.students.extend(biology101roster)
+
+
+    db.session.add_all(
+        [
+            deanPelton,
+            jeff,
+            abed,
+            britta,
+            shirley,
+            annie,
+            troy,
+            pierce,
+            starburns,
+            leonard,
+            vaughn,
+            garrett,
+            rich,
+            benChang,
+            ianDuncan,
+            michelleSlater,
+            seanGarrity,
+            juneBauer,
+            eusticeWhitman,
+            marshallKane,
+            spanish101,
+            psychology101,
+            statistics101,
+            accounting101,
+            anthropology101,
+            conspiracyTheories101,
+            biology101
+        ]
+    )
+
 
     db.session.commit()
+
+
+
+
+
+
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
