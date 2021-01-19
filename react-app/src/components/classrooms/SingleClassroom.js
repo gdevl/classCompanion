5f709a3341abaf5d076d87e3ef2439c632668cfa
gdevl <throwawayemailaddy@protonmail.com>
2021-01-18 20:33:42 -0500

    added clearClassrooms action to logout

--- react-app/src/components/classrooms/SingleClassroom.js
+++ react-app/src/components/classrooms/SingleClassroom.js
@@ -1,87 +1,102 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  getClassroomMeta,
-  fetchClassroomData,
-} from "../../store/classroom_meta";
-import UserCardContainer from "../InstructorClassroomDashboard/UserCard/UserCardContainer";
-import GroupCardContainer from "../InstructorClassroomDashboard/GroupCard/GroupCardContainer";
+    getClassroomMeta,
+    fetchClassroomData,
+} from '../../store/classroom_meta';
+import UserCardContainer from '../InstructorClassroomDashboard/UserCard/UserCardContainer';
+import GroupCardContainer from '../InstructorClassroomDashboard/GroupCard/GroupCardContainer';
 
 const SingleClassroom = () => {
-  const dispatch = useDispatch();
-  const currentUser = useSelector((state) => state.currentUser);
-  const currentClassroomId = useSelector((state) => state.currentClassroomId);
-  const classMeta = useSelector((state) => state.currentClassroomMeta);
-  const students = classMeta.students;
+    const [loaded, setLoaded] = useState(false);
+    const dispatch = useDispatch();
+    const currentUser = useSelector((state) => state.currentUser);
+    const currentClassroomId = useSelector((state) => state.currentClassroomId);
+    const classMeta = useSelector((state) => state.currentClassroomMeta);
+    const students = classMeta.students;
 
-  useEffect(() => {
-    (async () => {
-      const query = await fetchClassroomData(currentClassroomId);
-      dispatch(getClassroomMeta(query));
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const query = await fetchClassroomData(currentClassroomId);
+            dispatch(getClassroomMeta(query));
+        })();
+        setLoaded(true);
+    }, []);
 
-  if (!currentUser) return null;
-  if (!currentClassroomId) return null;
-  if (!classMeta) return null;
-  console.log("classMeta: ", classMeta);
-  console.log("students: ", students);
+    if (!currentUser) return null;
+    if (!currentClassroomId) return null;
+    if (!classMeta) return null;
+    console.log('classMeta: ', classMeta);
+    console.log('students: ', students);
+    console.log('loaded:');
+    console.log(loaded);
 
-  if (!classMeta.students) {
-    console.log("no students");
-    return (
-      <>
-        <div className="enroll-students-instructions-container">
-          <div>
-            <h3 className="enroll-students-instructions-heading">
-              You Have not yet Enrolled any Students
-            </h3>
-          </div>
-          <br></br>
-          <div>
-            <h3>
-              You Can Enroll Students by Clicking "My Classes" Followed by
-              "Enroll Students" on the Desired Class Card
-            </h3>
-          </div>
-        </div>
-      </>
-    );
-  }
-
-  //if there aren't any groups set, render everyone individually
-  if (!classMeta.groups.length >= 1) {
-    return (
-      <>
-        {classMeta.students.map((student) => {
-          return <UserCardContainer key={student.id} props={student} />;
-        })}
-      </>
-    );
-  }
-
-  //if groups are set, render group containers based on the size of the groups
-  if (classMeta.groups.length >= 1) {
-    let groups = [];
-    for (let i = 0; i < classMeta.groups.length; i++) {
-      groups.push(`Group ${i + 1}`);
+    if (!loaded) {
+        return <p>Loading...</p>;
     }
 
-    return (
-      <>
-        <GroupCardContainer props={{ groups, members: classMeta.groups }} />
-      </>
-    );
-  }
+    if (!classMeta.students) {
+        console.log('loaded:');
+        console.log(loaded);
+        console.log('no students');
+        return (
+            <>
+                <div className="enroll-students-instructions-container">
+                    <div>
+                        <h3 className="enroll-students-instructions-heading">
+                            You Have not yet Enrolled any Students
+                        </h3>
+                    </div>
+                    <br></br>
+                    <div>
+                        <h3>
+                            You Can Enroll Students by Clicking "My Classes"
+                            Followed by "Enroll Students" on the Desired Class
+                            Card
+                        </h3>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
-  //   return (
-  //     <>
-  //       <div className="classroom__details">
-  //         <h1>{classMeta.name}</h1>
-  //       </div>
-  //       <p>{console.log(currentClassroom)}</p>
-  //     </>
-  //   );
+    //if there aren't any groups set, render everyone individually
+    if (!classMeta.groups.length >= 1) {
+        return (
+            <>
+                {classMeta.students.map((student) => {
+                    return (
+                        <UserCardContainer key={student.id} props={student} />
+                    );
+                })}
+            </>
+        );
+    }
+
+    //if groups are set, render group containers based on the size of the groups
+    if (classMeta.groups.length >= 1) {
+        let groups = [];
+        for (let i = 0; i < classMeta.groups.length; i++) {
+            groups.push(`Group ${i + 1}`);
+        }
+
+        return (
+            <>
+                <GroupCardContainer
+                    props={{ groups, members: classMeta.groups }}
+                />
+            </>
+        );
+    }
+
+    //   return (
+    //     <>
+    //       <div className="classroom__details">
+    //         <h1>{classMeta.name}</h1>
+    //       </div>
+    //       <p>{console.log(currentClassroom)}</p>
+    //     </>
+    //   );
 };
 
 export default SingleClassroom;
--- react-app/src/store/classrooms.js
+++ react-app/src/store/classrooms.js
@@ -1,36 +1,46 @@
 // export const SET_CURRENT_CLASSROOM = "SET_CURRENT_CLASSROOM";
-export const GET_USER_CLASSROOMS = "GET_USER_CLASSROOMS";
+export const GET_USER_CLASSROOMS = 'GET_USER_CLASSROOMS';
+export const CLEAR_CLASSROOMS = 'CLEAR_CLASSROOMS';
 // export const GET_CLASSROOM_GROUPS = "GET_CLASSROOM_GROUPS";
 
 export const getUserClassrooms = (classrooms) => {
-  return {
-    type: GET_USER_CLASSROOMS,
-    classrooms,
-  };
+    return {
+        type: GET_USER_CLASSROOMS,
+        classrooms,
+    };
+};
+
+export const clearClassrooms = () => {
+    return {
+        type: CLEAR_CLASSROOMS,
+    };
 };
 
 export const fetchClassDisplay = async (userId) => {
-  const request = await fetch(`/api/users/${userId}/rooms`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    const request = await fetch(`/api/users/${userId}/rooms`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 
-  const response = await request.json();
-  console.log("response:");
-  console.log(response);
-  return response;
+    const response = await request.json();
+    console.log('response:');
+    console.log(response);
+    return response;
 };
 
 export default function reducer(state = {}, action) {
-  switch (action.type) {
-    case GET_USER_CLASSROOMS: {
-      return {
-        ...state,
-        ...action.classrooms,
-      };
+    switch (action.type) {
+        case GET_USER_CLASSROOMS: {
+            return {
+                ...state,
+                ...action.classrooms,
+            };
+        }
+        case CLEAR_CLASSROOMS: {
+            return {};
+        }
+        default:
+            return state;
     }
-    default:
-      return state;
-  }
 }
